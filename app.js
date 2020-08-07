746174cb45d0430311035cbd9d41d2b78995434b
salman754321 <salman754321@gmail.com>
2020-08-07 14:14:21 +0500

    Sessions Part 2

--- app.js
+++ app.js
@@ -38,48 +38,31 @@ app.use(session({
 }));
 
 function auth (req, res, next) {
-  if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
-        err.status = 401;
-        next(err);
-        return;
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == '123') {
-      req.session.user = 'admin';
-        next(); // authorized
-    } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');              
-        err.status = 401;
-        next(err);
-    }
+  if(!req.session.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+}
+else {
+  if (req.session.user === 'authenticated') {
+    next();
   }
   else {
-      if (req.session.user === 'admin') {
-          next();
-      }
-      else {
-          var err = new Error('You are not authenticated!');
-          err.status = 401;
-          next(err);
-      }
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
   }
 }
+}
 
-
+app.use('/users', usersRouter);
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/dish",dishRouter);
 app.use("/promo",PromoRouter);
 app.use("/leader" ,leaderRouter);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
