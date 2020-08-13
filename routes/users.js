b003fd50ecb4f70d40c4fb24e7e63026309db9b4
salman754321 <salman754321@gmail.com>
2020-08-13 12:58:35 +0500

    verify admin

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,17 @@ var passport = require('passport');
 var authenticate=require("../authenticate")
 router.use(bodyParser.json());
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content_type', 'application/json');
+      res.json(users);
+    }
+  })
 });
-
 router.post('/signup', (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
